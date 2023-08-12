9fc02be3f637fefade122758349bc43a1a2b164d
M.yusuke <soreiyu52@gmail.com>
2023-08-12 13:09:28 +0900

    first commit

---
+++ forge.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    packagerConfig: {
+        asar: true,
+    },
+    rebuildConfig: {},
+    publishers: [
+        {
+            name: "@electron-forge/publisher-github",
+            config: {
+                repository: {
+                    owner: "soreiyu52",
+                    name: "electron",
+                },
+                prerelease: false,
+                draft: true,
+            },
+        },
+    ],
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {},
+        },
+        {
+            name: "@electron-forge/maker-zip",
+            platforms: ["darwin"],
+        },
+        {
+            name: "@electron-forge/maker-deb",
+            config: {},
+        },
+        {
+            name: "@electron-forge/maker-rpm",
+            config: {},
+        },
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                certificateFile: "./cert.pfx",
+                certificatePassword: process.env.CERTIFICATE_PASSWORD,
+            },
+        },
+    ],
+    plugins: [
+        {
+            name: "@electron-forge/plugin-auto-unpack-natives",
+            config: {},
+        },
+    ],
+};
